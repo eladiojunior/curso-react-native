21eee6fff345da0bc44fbb0a40ecb0247dcc23fe
Eladio Júnior <eladiojunior@gmail.com>
2022-01-17 20:53:01 -0300

    Implementação da lista em função

--- src/componentes/produtos/ListaProdutos.js
+++ src/componentes/produtos/ListaProdutos.js
@@ -5,12 +5,17 @@ import Estilo from '../estilo';
 import produtos from './dataProdutos';
 
 export default (props) => {
+    function listarProdutos() {
+        return produtos.map(p => {
+            return (
+                <Text key={p.id}>{p.id} - {p.nome} R$ {p.preco}</Text>
+            );
+        });
+    }
     return (
         <View>
             <Text style={Estilo.textoGrande}>Lista de Produtos</Text>
-            {produtos.map(p => {
-                return <Text key={p.id}>{p.id} - {p.nome} R$ {p.preco}</Text>
-            })}
+            {listarProdutos()}
         </View>
     );
 }
