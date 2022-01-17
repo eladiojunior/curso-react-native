68b0bbd78e5ac7c2656f99016c66efe9a68bf989
Eladio Júnior <eladiojunior@gmail.com>
2022-01-17 20:49:58 -0300

    Lista de produtos.

--- src/componentes/produtos/ListaProdutos.js
+++ src/componentes/produtos/ListaProdutos.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import Estilo from '../estilo';
+
+import produtos from './dataProdutos';
+
 export default (props) => {
     return (
         <View>
             <Text style={Estilo.textoGrande}>Lista de Produtos</Text>
+            {produtos.map(p => {
+                return <Text key={p.id}>{p.id} - {p.nome} R$ {p.preco}</Text>
+            })}
         </View>
     );
 }
